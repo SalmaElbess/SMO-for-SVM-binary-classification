468bf60a04f8e21f78af34cad6a24b3f96c96aa3
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2021-01-28 21:36:23 +0200

    Update and rename compute_eta_sayed.m to compute_eta_smo.m

--- compute_eta_smo.m
+++ compute_eta_smo.m
@@ -1,11 +1,12 @@
-function [eta,K11,K12,K22] = compute_eta_sayed(x1,x2)
+function [eta,K11,K12,K22] = compute_eta_smo(x1,x2)
 %COMPUTE_ETA:compute the second derivative using 2 alphas
 % Inputs: 
     % x1,x2:         2 features vectors.
 % Outputs: 
     %eta:            The 2nd derivative
+    %K11,K12,K22
     
-    % each x is a row
+    
  %check 
  if size(x1,1) ~= 1
      x1 = x1'; x2= x2';
