d7ccf38cc894ea257d79f848630e8ac7941901d6
ElSayedMMostafa <45875353+ElSayedMMostafa@users.noreply.github.com>
2021-01-28 21:38:00 +0200

    Update predict_smo.m

--- predict_smo.m
+++ predict_smo.m
@@ -1,5 +1,19 @@
 function [y_p,error] = predict_smo(w,b,x_test,y_test)
+%PERDICT_SMO: This function uses the SVM hyperplane weight with the test
+%data to generate the output by SVM equation and calucaltes the output
+%missclassified output depending on the output target values.
 
+% Inputs: 
+    % w:             The svm equation weights (d x 1)
+    % b:             The svm equation threshold
+    % x:             The features matrix (d x N)   
+    % x_test:        The test data   
+    % y_test:        The output target data  
+% Outputs: 
+    % y_p:           The predicted classes
+    % error:         The number of missclassified examples.
+
+    
 % dimensions:
  % x ( d x N )
  % y ( N x 1 )
